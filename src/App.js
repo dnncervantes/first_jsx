2f4797249400a22dbd617b70392eb4b7373d7888
Danniel C <dnncervantes.yahoo.com>
2022-05-04 13:13:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="my-class">Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
     </div>
   );
 }
